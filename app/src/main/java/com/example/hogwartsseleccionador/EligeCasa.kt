61575e13f6a7b7681cf59e85f26f1f8dfbd3ac50
Gabriel <gabrodru@gmail.com>
2023-11-21 17:06:18 +0100

    feat: Add blinking animation to student count display and update text style 🚦✨

--- app/src/main/java/com/example/hogwartsseleccionador/EligeCasa.kt
+++ app/src/main/java/com/example/hogwartsseleccionador/EligeCasa.kt
@@ -3,11 +3,13 @@ package com.example.hogwartsseleccionador
 import CasaHogwarts
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
-
+import androidx.core.content.ContextCompat
 class EligeCasa : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,12 +22,11 @@ class EligeCasa : AppCompatActivity() {
         val txtNumHufflepuff: TextView = findViewById(R.id.txtNumHufflepuff)
         val txtNumRavenclaw: TextView = findViewById(R.id.txtNumRavenclaw)
 
-        // Obtenemos los números de la BBDD de SQLite
-        var hogwartsDB: HogwartsDatabaseHelper = HogwartsDatabaseHelper(this)
-        txtNumGriffindor.text = hogwartsDB.getNumeroAlumnos(CasaHogwarts.CASA_GRIFFINDORF).toString() + " alumnos"
-        txtNumSlytherin.text = hogwartsDB.getNumeroAlumnos(CasaHogwarts.CASA_SLYTHERIN).toString() + " alumnos"
-        txtNumHufflepuff.text = hogwartsDB.getNumeroAlumnos(CasaHogwarts.CASA_HUFFLEPLUFF).toString() + " alumnos"
-        txtNumRavenclaw.text = hogwartsDB.getNumeroAlumnos(CasaHogwarts.CASA_RAVENCLAW).toString() + " alumnos"
+        // Actualizar conteo de alumnos y color del texto si es necesario
+        updateStudentCount(txtNumGriffindor, CasaHogwarts.CASA_GRIFFINDORF)
+        updateStudentCount(txtNumSlytherin, CasaHogwarts.CASA_SLYTHERIN)
+        updateStudentCount(txtNumHufflepuff, CasaHogwarts.CASA_HUFFLEPLUFF)
+        updateStudentCount(txtNumRavenclaw, CasaHogwarts.CASA_RAVENCLAW)
 
         // Botón y acción para Griffindor
         val btnGriffindor: Button = findViewById(R.id.btnVerGriffindorf)
@@ -57,6 +58,20 @@ class EligeCasa : AppCompatActivity() {
             val intent = Intent(this, ListadoCasaHogwarts::class.java)
             intent.putExtra("casa", CasaHogwarts.CASA_RAVENCLAW)
             startActivity(intent)
+        }
+
+    }
+
+    private fun updateStudentCount(textView: TextView, house: String) {
+        val blinkAnimation = AnimationUtils.loadAnimation(this, R.anim.blink)
+        var hogwartsDB: HogwartsDatabaseHelper = HogwartsDatabaseHelper(this)
+        val count = hogwartsDB.getNumeroAlumnos(house)
+        textView.text = "$count alumnos"
+        if (count > 0) {
+            textView.setTextColor(ContextCompat.getColor(this, R.color.black))
+            textView.setTypeface(null, Typeface.BOLD)
+            textView.startAnimation(blinkAnimation)
+            return
         }
 
     }
