eb8e3e2d7346070fc09c07b3631e43357d295cb4
Gabriel <gabrodru@gmail.com>
2023-11-21 13:34:34 +0100

    Implement name validation for new student entry

--- app/src/main/java/com/example/hogwartsseleccionador/NuevoAlumno.kt
+++ app/src/main/java/com/example/hogwartsseleccionador/NuevoAlumno.kt
@@ -23,26 +23,32 @@ class NuevoAlumno : AppCompatActivity() {
             //Cogemos los valores de los formularios
             val txtNombre: EditText = findViewById(R.id.txtNombreAlumnoNuevo)
             val esMaggle: CheckBox = findViewById(R.id.chkFamilia)
-            // Debe introducir un nombre válido
-            if (txtNombre.text.toString() == "Nombre")
-                Toast.makeText(this, "Debes introducir un nombre válido", Toast.LENGTH_SHORT)
-            else{
-                val nombre = txtNombre.text.toString()
-                val maggle = esMaggle.isChecked
-                val nuevoAlumno: AlumnoHogwarts = AlumnoHogwarts(nombre, maggle)
-                // Acceso a la BBDD de SQLite
-                var hogwartsDB: HogwartsDatabaseHelper = HogwartsDatabaseHelper(this)
-                val idAlumno: Long = hogwartsDB.insert(nuevoAlumno)
-                hogwartsDB.close()
-                // Nos vamos a la siguiente Activity
-                var intent = Intent(this, BienvenidaAlumno::class.java)
-                startActivity(intent)
+            val maggle = esMaggle.isChecked
+            val nombre = txtNombre.text.toString().trim()
+
+            // Si el nombre está en blanco o no contiene un espacio, muestra un Toast y termina el método aquí.
+            if (nombre.isBlank() || !nombre.contains(" ")) {
+                Toast.makeText(
+                    this, "Introduce nombre y apellido separados por un espacio.", Toast.LENGTH_LONG
+                ).show()
+                return@setOnClickListener
             }
+
+
+            val nuevoAlumno: AlumnoHogwarts = AlumnoHogwarts(nombre, maggle)
+            // Acceso a la BBDD de SQLite
+            var hogwartsDB: HogwartsDatabaseHelper = HogwartsDatabaseHelper(this)
+            val idAlumno: Long = hogwartsDB.insert(nuevoAlumno)
+            hogwartsDB.close()
+            // Nos vamos a la siguiente Activity
+            var intent = Intent(this, BienvenidaAlumno::class.java)
+            startActivity(intent)
+
         }
 
         // Imagen btnAtras
         val btnImgAtras: ImageButton = findViewById(R.id.imgBtnAtras)
-        btnImgAtras.setOnClickListener{
+        btnImgAtras.setOnClickListener {
             // Nos devuelve a la MainActivity
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
